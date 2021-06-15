e61cee41824ec2b3586fc7c227ad516a5e6cca7d
dev-bhaskar8 <43923435+dev-bhaskar8@users.noreply.github.com>
2021-06-15 09:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from flask_limiter.util import get_remote_address
 from flask import request
 import requests
 
-
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"}
-
 
 app = Flask(__name__)
 limiter = Limiter(
@@ -18,8 +16,7 @@ limiter = Limiter(
 @limiter.limit("5 per day")
 def api():
     address = request.args['address']
-    response = requests.get(f"http://lunacia.skymavis.com/game-api/clients/{address}/items/1",headers=headers)
-    response=response.json()
+    response = requests.get(f"http://lunacia.skymavis.com/game-api/clients/{address}/items/1",headers=headers).json()
     try:
         return response
     except KeyError:
